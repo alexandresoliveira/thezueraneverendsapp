01b2da77a6a26308cc5057118823dc3b9b4be49c
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-01-17 11:29:21 -0300

    First commit

---
+++ src/main/java/dev/aleoliv/thezueranetwork/thezueraneverendsapp/ThezueraneverendsappApplication.java
@@ -0,0 +1,13 @@
+package dev.aleoliv.thezueranetwork.thezueraneverendsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThezueraneverendsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThezueraneverendsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aleoliv/thezueranetwork/thezueraneverendsapp/ThezueraneverendsappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aleoliv.thezueranetwork.thezueraneverendsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThezueraneverendsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
