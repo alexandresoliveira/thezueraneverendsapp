e9e899ba208cbc044f12bbbf240985731221ac2a
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-01-19 11:05:46 -0300

    Change repository

--- src/main/java/dev/aleoliv/thezueranetwork/thezueraneverendsapp/usecases/v1/phrases/create/services/CreatePhraseService.java
+++ src/main/java/dev/aleoliv/thezueranetwork/thezueraneverendsapp/usecases/v1/phrases/create/services/CreatePhraseService.java
@@ -1,42 +1,43 @@
 package dev.aleoliv.thezueranetwork.thezueraneverendsapp.usecases.v1.phrases.create.services;
 
+import org.springframework.stereotype.Service;
+
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.shared.database.entities.PhraseEntity;
-import dev.aleoliv.thezueranetwork.thezueraneverendsapp.shared.database.repositories.PhraseRepository;
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.shared.database.repositories.UserRepository;
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.shared.exceptions.ServiceApiException;
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.usecases.v1.phrases.create.dtos.CreatePhraseRequestDTO;
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.usecases.v1.phrases.create.dtos.CreatePhraseResponseDTO;
-import org.springframework.stereotype.Service;
+import dev.aleoliv.thezueranetwork.thezueraneverendsapp.usecases.v1.phrases.create.repositories.CreatePhraseRepository;
 
 @Service
 public class CreatePhraseService {
 
-    private final PhraseRepository phraseRepository;
-    private final UserRepository userRepository;
+  private final CreatePhraseRepository createPhraseRepository;
+  private final UserRepository userRepository;
 
-    public CreatePhraseService(PhraseRepository phraseRepository, UserRepository userRepository) {
-        this.phraseRepository = phraseRepository;
-        this.userRepository = userRepository;
+  public CreatePhraseService(CreatePhraseRepository createPhraseRepository, UserRepository userRepository) {
+    this.createPhraseRepository = createPhraseRepository;
+    this.userRepository = userRepository;
+  }
+
+  public CreatePhraseResponseDTO execute(CreatePhraseRequestDTO requestDTO) {
+
+    var optionalUser = userRepository.findById(requestDTO.getUserId());
+
+    if (!optionalUser.isPresent()) {
+      throw new ServiceApiException("User don't exists.");
     }
 
-    public CreatePhraseResponseDTO execute(CreatePhraseRequestDTO requestDTO) {
+    var optionalPhrase = createPhraseRepository.findByTextAndAuthor(requestDTO.getText(), optionalUser.get());
 
-        var optionalUser = userRepository.findById(requestDTO.getUserId());
-
-        if (!optionalUser.isPresent()) {
-            throw new ServiceApiException("User don't exists.");
-        }
-
-        var optionalPhrase = phraseRepository.findByTextAndAuthor(requestDTO.getText(), optionalUser.get());
-
-        if (optionalPhrase.isPresent()) {
-            throw new ServiceApiException("Phrase exists!");
-        }
-
-        var entity = new PhraseEntity(requestDTO.getText(), optionalUser.get());
-
-        var phraseEntity = phraseRepository.saveAndFlush(entity);
-
-        return new CreatePhraseResponseDTO(phraseEntity.getText(), phraseEntity.getAuthor().getName());
+    if (optionalPhrase.isPresent()) {
+      throw new ServiceApiException("Phrase exists!");
     }
+
+    var entity = new PhraseEntity(requestDTO.getText(), optionalUser.get());
+
+    var phraseEntity = createPhraseRepository.saveAndFlush(entity);
+
+    return new CreatePhraseResponseDTO(phraseEntity.getText(), phraseEntity.getAuthor().getName());
+  }
 }
