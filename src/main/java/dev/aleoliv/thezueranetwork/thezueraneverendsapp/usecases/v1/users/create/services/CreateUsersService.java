fdd60f1974196f693fa9bd8ca0d0ab4d79f230b5
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-01-19 22:40:00 -0300

    Refactory methods on create user service

--- src/main/java/dev/aleoliv/thezueranetwork/thezueraneverendsapp/usecases/v1/users/create/services/CreateUsersService.java
+++ src/main/java/dev/aleoliv/thezueranetwork/thezueraneverendsapp/usecases/v1/users/create/services/CreateUsersService.java
@@ -1,43 +1,41 @@
 package dev.aleoliv.thezueranetwork.thezueraneverendsapp.usecases.v1.users.create.services;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.shared.database.entities.UserEntity;
-import dev.aleoliv.thezueranetwork.thezueraneverendsapp.shared.database.repositories.UserRepository;
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.shared.exceptions.ServiceApiException;
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.usecases.v1.users.create.dtos.CreateUsersRequestDTO;
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.usecases.v1.users.create.dtos.CreateUsersResponseDTO;
 import dev.aleoliv.thezueranetwork.thezueraneverendsapp.usecases.v1.users.create.repositories.CreateUserRepository;
 
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
 @Service
 public class CreateUsersService {
 
-    private final CreateUserRepository createUserRepository;
+  private final CreateUserRepository createUserRepository;
 
-    public CreateUsersService(CreateUserRepository createUserRepository) {
-        this.createUserRepository = createUserRepository;
+  public CreateUsersService(CreateUserRepository createUserRepository) {
+    this.createUserRepository = createUserRepository;
+  }
+
+  public CreateUsersResponseDTO execute(CreateUsersRequestDTO requestDTO) {
+    var optionalUserEntity = createUserRepository.findByEmail(requestDTO.getEmail());
+
+    if (optionalUserEntity.isPresent()) {
+      throw new ServiceApiException(
+          String.format("Exists user with this email (%s)", optionalUserEntity.get().getEmail()));
     }
 
-    public CreateUsersResponseDTO execute(CreateUsersRequestDTO requestDTO) {
-        Optional<UserEntity> optionalUserEntity = createUserRepository.findByEmail(requestDTO.getEmail());
+    var entity = new UserEntity();
+    entity.setName(requestDTO.getName());
+    entity.setEmail(requestDTO.getEmail());
+    entity.setPassword(requestDTO.getPassword());
 
-        if (optionalUserEntity.isPresent()) {
-            throw new ServiceApiException(
-                    String.format("Exists user with this email (%s)", optionalUserEntity.get().getEmail()));
-        }
+    var savedEntity = createUserRepository.saveAndFlush(entity);
 
-        UserEntity entity = new UserEntity();
-        entity.setName(requestDTO.getName());
-        entity.setEmail(requestDTO.getEmail());
-        entity.setPassword(requestDTO.getPassword());
+    var responseDTO = new CreateUsersResponseDTO(savedEntity.getId(), savedEntity.getName(), savedEntity.getEmail());
 
-        UserEntity savedEntity = createUserRepository.saveAndFlush(entity);
-
-        var responseDTO = new CreateUsersResponseDTO(savedEntity.getId(), savedEntity.getName(),
-                savedEntity.getEmail());
-
-        return responseDTO;
-    }
+    return responseDTO;
+  }
 }
