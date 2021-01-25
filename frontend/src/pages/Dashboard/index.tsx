d23fe84fc7ad9d57608a4b48802ec75453bdf42d
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-01-24 23:36:45 -0300

    Building a dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+
+import { Container, CardInfo } from './styled';
+
+import FloatingButton from '../../components/FloatingButton';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <CardInfo>
+        <p>Número de frases</p>
+        <span>5</span>
+      </CardInfo>
+      <CardInfo>
+        <p>Aprovações pendentes</p>
+        <span>5</span>
+      </CardInfo>
+      <FloatingButton icon={FiPlus} />
+    </Container>
+  );
 };
 
 export default Dashboard;
--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -25,5 +25,6 @@ export default createGlobalStyle`
     max-width: 800px;
     margin: 0 auto;
     padding: 40px 20px;
+    min-height: 100vh;
   }
 `;
